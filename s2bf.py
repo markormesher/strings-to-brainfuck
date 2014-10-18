6264f0abb39bc5d85e021dbe975908cc7c00e617
Mark Ormesher <me@markormesher.co.uk>
2014-10-18 01:29:36 +0100

    Improvement: loop used to move to square number before target when target >= 16

--- s2bf.py
+++ s2bf.py
@@ -1,4 +1,10 @@
 import sys
+import math
+
+
+# cell locations
+COUNTER_CELL = 0
+OUTPUT_CELL = 1
 
 
 current_cell_value = 0
@@ -6,8 +12,22 @@ current_cell = 0
 output = ""
 
 
-def get_cell_move_string(moveTo):
-	pass
+def move_to_cell(moveTo):
+	global current_cell, output
+
+	# how far to move
+	difference = moveTo - current_cell
+
+	# move!
+	if (difference > 0):
+		for i in range(0, difference):
+			output += ">"
+	elif (difference < 0):
+		for i in range(0, (difference * -1)):
+			output += "<"
+
+	# update current location
+	current_cell = moveTo
 
 
 def change_value(changeTo):
@@ -15,14 +35,44 @@ def change_value(changeTo):
 
 	# how far to move?
 	difference = changeTo - current_cell_value
+	if (difference == 0):
+		pass
+	elif (difference > 0):
+		moveChar = "+"
+	else:
+		difference = difference * -1
+		moveChar = "-"
 
-	# inefficient right now
-	if (difference > 0):
-		for i in range(0, difference):
+	# pick which method to use
+	if (difference >= 16):
+		# use a loop to change by the biggest square number smaller than the target, then adjust
+
+		# move into the loop counter and set it up
+		move_to_cell(COUNTER_CELL)
+		root = int(math.floor(math.sqrt(difference)))
+		for i in range(0, root):
 			output += "+"
-	elif (difference < 0):
-		for i in range(0, (difference * -1)):
-			output += "-"
+
+		# create the loop section: move to output, increase, back to counter, decrease
+		output += "["
+		move_to_cell(OUTPUT_CELL)
+		for i in range(0, root):
+			output += moveChar
+		move_to_cell(COUNTER_CELL)
+		output += "-]"
+		move_to_cell(OUTPUT_CELL)
+
+		# add on anything missed in the loop
+		extra = difference - (root * root)
+		for i in range(0, extra):
+			output += moveChar
+	else:
+		# inefficient linear method
+		for i in range(0, difference):
+			output += moveChar
+
+	# update current cell value
+	current_cell_value = changeTo
 
 
 def translate(input):
@@ -31,17 +81,17 @@ def translate(input):
 	# loop all characters
 	characters = list(input)
 	for c in characters:
-		# find the ASCII value for this colour
+		# find the ASCII value for this character
 		required_char_val = ord(c)
 
 		# apply the necessary changes
 		change_value(required_char_val)
 
+		# make sure we're on output
+		move_to_cell(OUTPUT_CELL)
+
 		# print the character
 		output += "."
-
-		# update our memory of the current cell value
-		current_cell_value = required_char_val
 
 	# return the BF
 	return output
